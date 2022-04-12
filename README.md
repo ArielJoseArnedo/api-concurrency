9cf7ad4364bb0fbcd3f074819fa2c6945e662169
Ariel Arnedo <ariel_arnedo@epam.com>
2022-04-11 20:53:38 -0500

    se agregan mas funcionalidades a la clase y se realizan test unitarios

---
+++ README.md
@@ -0,0 +1,5 @@
+Api-Concurrency
+======================
+
+### Description
+This library contains the base entities to implement a CQPS component.
--- lib/src/main/java/co/com/ajac/concurrency/FutureEither.java
+++ lib/src/main/java/co/com/ajac/concurrency/FutureEither.java
@@ -7,24 +7,59 @@ import io.vavr.control.Either;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Getter
 @AllArgsConstructor
 public class FutureEither<L, R> {
 
-    public static <L, R> FutureEither<L, R> of(Future<Either<L, R>> value) { return new FutureEither<>(value); }
+    public static <L, R> FutureEither<L, R> of(Future<Either<L, R>> value) {
+        return new FutureEither<>(value);
+    }
 
-    public static <L, R> FutureEither<L, R> fromEither(Either<L, R> either) { return new FutureEither<>(Future.successful(either)); }
+    public static <L, R> FutureEither<L, R> fromEither(Either<L, R> either) {
+        return new FutureEither<>(Future.successful(either));
+    }
 
-    public static <L, R> FutureEither<L, R> right(R r) { return new FutureEither<>(Future.successful(Either.right(r))); }
+    public static <L, R> FutureEither<L, R> right(R r) {
+        return new FutureEither<>(Future.successful(Either.right(r)));
+    }
 
-    public static <L, R> FutureEither<L, R> left(L l) { return new FutureEither<>(Future.successful(Either.left(l))); }
+    public static <L, R> FutureEither<L, R> left(L l) {
+        return new FutureEither<>(Future.successful(Either.left(l)));
+    }
 
     private final Future<Either<L, R>> value;
 
     public <U> FutureEither<L, U> map(Function1<R, U> f) {
         return new FutureEither<>(this.value.map(e -> e.map(f)));
+    }
+
+    public <U> FutureEither<L, U> flatMap(Function1<R, FutureEither<L, U>> f) {
+        return new FutureEither<>(
+          this.value.flatMap(either -> either
+            .fold(
+              left -> Future.successful(Either.left(left)),
+              right -> f.apply(right).getValue()
+            )
+          ));
+    }
+
+    public <U> FutureEither<U, R> flatMapLeft(Function1<L, FutureEither<U, R>> f) {
+        return new FutureEither<>(
+          this.value.flatMap(either -> either
+            .fold(
+              left -> f.apply(left).getValue(),
+              right -> Future.successful(Either.right(right))
+            )
+          ));
+    }
+
+    public <U> FutureEither<U, R> mapLeft(Function<? super L, ? extends U> leftMapper) {
+        Objects.requireNonNull(leftMapper, "leftMapper is null");
+        return new FutureEither<>(value.map(either -> either.mapLeft(leftMapper)));
     }
 
     public <U> FutureEither<L, U> mapTry(CheckedFunction1<R, U> mapper) {
@@ -51,10 +86,12 @@ public class FutureEither<L, R> {
         return new FutureEither<>(res);
     }
 
-    public <U> FutureEither<L, U> flatMap(Function1<R, FutureEither<L, U>> f) {
-        return new FutureEither<>(
-          this.value.flatMap(
-            e -> e.fold(l -> Future.successful(Either.left(l)), r -> f.apply(r).getValue())));
+    public <U> FutureEither<L, U> flatMapFuture(Function<? super R, ? extends Future<Either<L, U>>> mapper) {
+        return new FutureEither<>(value.flatMap(either -> either
+          .fold(
+            left -> Future.successful(Either.left(left)),
+            mapper
+          )));
     }
 
     public <C> Future<C> fold(Function1<L, C> fl, Function1<R, C> fr) {
@@ -75,7 +112,22 @@ public class FutureEither<L, R> {
         return this.peekLeft(leftConsumer).peek(rightConsumer);
     }
 
+    public <X, Y> FutureEither<X, Y> bimap(Function<? super L, ? extends X> leftMapper, Function<? super R, ? extends Y> rightMapper) {
+        Objects.requireNonNull(leftMapper, "leftMapper is null");
+        Objects.requireNonNull(rightMapper, "rightMapper is null");
+        return new FutureEither<>(value.map(either -> either.bimap(leftMapper, rightMapper)));
+    }
+
+    public FutureEither<L, R> recover(Function<? super Throwable, ? extends Either<L, R>> function) {
+        Objects.requireNonNull(function, "function is null");
+        return new FutureEither<>(value.recover(function));
+    }
+
     public FutureEither<L, R> onFailure(Consumer<? super Throwable> consumer) {
         return new FutureEither<>(this.value.onFailure(consumer));
+    }
+
+    public FutureEither<L, R> onSuccess(Consumer<? super Either<L, R>> consumer) {
+        return new FutureEither<>(this.value.onSuccess(consumer));
     }
 }
